0c1cc93f4a118ef20cb91d91d97f65181c115c54
keerthana <keerthi2414@gmail.com>
2017-06-08 15:46:04 +0530

    added testing for eureka service

---
+++ discovery-registry/src/test/java/hello/EurekaServiceApplicationTests.java
@@ -0,0 +1,31 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+public class EurekaServiceApplicationTests {
+
+	@Autowired
+	DiscoveryClient discoveryClient;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void discoveryClientIsEureka() {
+		assertTrue("discoveryClient is wrong type: " + discoveryClient, discoveryClient instanceof EurekaDiscoveryClient);
+	}
+
+}
