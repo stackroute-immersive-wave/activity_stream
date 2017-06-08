d4e1fe902231233dc2ce3048cad5fae8e20f6761
Charu <charubhatt40@gmail.com>
2017-06-08 14:02:55 +0530

    unit testing added for config server

--- configuration-client/src/test/java/com/stackroute/configurationclient/ConfigurationClientApplicationTests.java
+++ configuration-client/src/test/java/com/stackroute/configurationclient/ConfigurationClientApplicationTests.java
@@ -1,16 +1,25 @@
-package com.stackroute.configurationclient;
+package com.stackroute.configurationserver;
+
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigurationClientApplicationTests {
+public class ConfigurationServerApplicationTests {
+	
+	@Autowired
+	ConfigurableEnvironment environment;
 
 	@Test
 	public void contextLoads() {
+		assertFalse(this.environment.getPropertySources().contains("application"));
 	}
-
 }
+
